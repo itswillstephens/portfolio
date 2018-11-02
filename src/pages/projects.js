c936d25cec07cf2d9f724a6eff63f419c168de82
Will Stephens <wssdesigns@gmail.com>
2018-11-02 12:47:22 -0400

    Add wssdesigns project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import GoodStreams from '../images/goodstreams.png'
 import BeerMenus from '../images/beermenus.png'
+import Wssdesigns from '../images/wssdesigns.png'
 
 const Projects = () => (
   <Layout>
@@ -10,7 +11,7 @@ const Projects = () => (
     <div style={{fontWeight: '900'}}>BeerMenus</div>
     <div>March 2008 - present</div>
     <br/>
-    <p><a href="https://www.beermenus.com" style={{color: 'blue'}}>BeerMenus</a> is the largest platform for finding craft beer with >40,000 participating businesses worldwide and millions of craft beer fans visiting each month.</p>
+    <p>I co-founded <a href="https://www.beermenus.com" style={{color: 'blue'}}>BeerMenus</a>, which is the largest platform for finding craft beer with >40,000 participating businesses worldwide and millions of craft beer fans visiting each month.</p>
 
     <img
       src={BeerMenus}
@@ -49,7 +50,25 @@ const Projects = () => (
         <li>Node</li>
         <li>Bootstrap</li>
       </ul>
-
+      <br/>
+    <hr/>
+    <div style={{fontWeight: '900'}}>wssdesigns</div>
+    <div>October 2018 - present</div>
+    <br/>
+    <p>This is the site you're on now! I use this to test and try out new Customer Success and Analytics tools (see below).</p>
+    <img
+      src={Wssdesigns}
+      alt="wssdesigns logo"
+    >
+    </img>
+    <div>Technologies:</div>
+      <ul style={{paddingLeft: '20px'}}>
+        <li><a href="https://www.gatsbyjs.org/" style={{color: 'blue'}}>Gatsby.js</a>: I used this progressive web app generator to build this site. It runs on the React frontend framework with webpack to manage dependencies.</li>
+        <li><a href="https://analytics.google.com/analytics/web/
+        " style={{color: 'blue'}}>Google Analytics</a>: I of course have GA running to better understand the traffic that comes to this site.</li>
+        <li><a href="https://www.fullstory.com/" style={{color: 'blue'}}>FullStory</a>: I use this session replay service to better understand the customer journey.</li>
+        <li><a href="https://www.drift.com" style={{color: 'blue'}}>Drift</a>: I use this to power the chat bubble at the bottom right to make it easy for you to get in touch with me.</li>
+      </ul>
   </Layout>
 )
 
