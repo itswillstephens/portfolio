016c0ee5e620e0c582b54e9cc5a46b9bea1a662b
Will Stephens <wssdesigns@gmail.com>
2018-11-02 12:10:48 -0400

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,12 +26,12 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
-  ],
-  {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-    // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-      trackingId: "UA-128575923-1",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+      // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-128575923-1",
+      },
     },
-  },
+  ],
 }
