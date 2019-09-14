038e8a04d88721787e50d76d719413bc24b34e4f
wssdesigns <32200792+wssdesigns@users.noreply.github.com>
2019-09-14 11:23:08 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,13 @@ const IndexPage = () => (
 
     <h1>Welcome</h1>
 
-    <p>I'm an entrepreneur, customer success manager, and beer brewer.</p>
+    <p>I'm an entrepreneur, product manager, and beer brewer.</p>
 
-    <p>I've spent my career working at BeerMenus, a company <a href="https://www.beermenus.com/about" style={{color: 'blue'}}>I created with my brother</a>.</p>
+    <p>I started my career co-founding BeerMenus, a company <a href="https://www.beermenus.com/about" style={{color: 'blue'}}>I created with my brother</a>.</p>
 
-    <YouTube id='HpDqzJfvkw4' />
-    <div style={{fontSize: '0.7rem', textAlign: 'center'}}>A video HelpScout made about the BeerMenus Success team</div>
-    <br/>
-
-    <p>I recently started teaching myself full stack web development to learn how to better scale and automate processes for support, sales, and customer success teams. I'm currently working on a side project <a href="https://www.goodstreams.io/" style={{color: 'blue'}}>GoodStreams</a> which I developed to learn the MERN (Mongo, Express, React, Node) stack and to continually learn new technologies.</p>
+    <p>I now work at <a href="https://go.sevenfifty.com/">SevenFifty</a>.</p> 
+  
+    <p>I'm a <a href="https://www.cicerone.org/us-en/users/will-stephens">Certified Cicerone</a>, Recognized Judge with the <a href="https://www.bjcp.org/">BJCP program</a>, and I've earned the <a href="https://wsetglobal.com/qualifications/wset-level-2-award-in-wines/">WSET Level 2</a> certification with distinction. </p>
 
     <p>Thanks for stopping by.</p>
 
